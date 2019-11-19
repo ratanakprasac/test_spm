6c250e4efd26264d290a14943c4059dcbcac26fc
Keo Ratanak <ratanak.keo@prasac.com.kh>
2019-11-19 09:00:09 +0700

    Initialize project

---
+++ TestSPMCore/TestSPMCore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSPMCore
+//
+//  Created by Keo Ratanak on 11/19/19.
+//  Copyright © 2019 Keo Ratanak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
